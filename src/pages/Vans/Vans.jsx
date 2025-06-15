d175b068452d79f1811382fe77b1d87843e5e21d
S <sarimansari313@gmail.com>
2025-06-16 00:04:47 +0530

    useLocation

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -19,7 +19,13 @@ export default function Vans() {
 
   const vanElements = displayedVans.map((van) => (
     <div key={van.id} className="van-tile">
-      <Link to={`/vans/${van.id}`}>
+      <Link
+        to={van.id}
+        state={{
+          search: `?${searchParams.toString()}`,
+          type: typeFilter,
+        }}
+      >
         <img alt={van.name} src={van.imageUrl} />
         <div className="van-info">
           <h3>{van.name}</h3>
@@ -44,45 +50,28 @@ export default function Vans() {
     });
   }
 
-  /**
-   * Challenges:
-   * 1. Conditionally render the "Clear filter" button only if
-   *    there's a `type` filter currently applied in the search params
-   *
-   * 2. On just the 3 filter buttons (not the Clear filter button),
-   *    conditionally render the className "selected" if the
-   *    typeFilter value equals the value that button sets it to.
-   *    (We don't have a variable for that, so it'll be a hard-coded
-   *    string).
-   *
-   *    Hint: `...${typeFilter === "simple" ? ...}`
-   */
-
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list-filter-buttons">
         <button
           onClick={() => handleFilterChange("type", "simple")}
-          className={`van-type simple ${
-            typeFilter === "simple" ? "selected" : ""
-          }`}
+          className={`van-type simple 
+                        ${typeFilter === "simple" ? "selected" : ""}`}
         >
           Simple
         </button>
         <button
           onClick={() => handleFilterChange("type", "luxury")}
-          className={`van-type luxury ${
-            typeFilter === "luxury" ? "selected" : ""
-          }`}
+          className={`van-type luxury 
+                        ${typeFilter === "luxury" ? "selected" : ""}`}
         >
           Luxury
         </button>
         <button
           onClick={() => handleFilterChange("type", "rugged")}
-          className={`van-type rugged ${
-            typeFilter === "rugged" ? "selected" : ""
-          }`}
+          className={`van-type rugged 
+                        ${typeFilter === "rugged" ? "selected" : ""}`}
         >
           Rugged
         </button>
