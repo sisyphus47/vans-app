a1dffb9aaafe83258df2d0ca330d7769e3f3493d
S <sarimansari313@gmail.com>
2025-06-16 19:27:49 +0530

    final

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -6,12 +6,12 @@ import {
   Outlet,
   useLoaderData,
 } from "react-router-dom";
-import { getHostVans } from "../../api";
+import { getVan } from "../../api";
 import { requireAuth } from "../../utils";
 
 export async function loader({ params, request }) {
   await requireAuth(request);
-  return getHostVans(params.id);
+  return getVan(params.id);
 }
 
 export default function HostVanDetail() {
